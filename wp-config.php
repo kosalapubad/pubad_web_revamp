7c3b61ba3c7af1bc80a1d741a65d3f8dc6a0266d
kosalapubad <kosala.pubad@gmail.com>
2026-06-29 21:09:46 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,27 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'pubad' );
+//define( 'DB_NAME', 'pubad' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+//define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+//define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+//define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+//define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+//define( 'DB_COLLATE', '' );
+
+define('DB_NAME', getenv('MYSQLDATABASE'));
+define('DB_USER', getenv('MYSQLUSER'));
+define('DB_PASSWORD', getenv('MYSQLPASSWORD'));
+define('DB_HOST', getenv('MYSQLHOST') . ':' . getenv('MYSQLPORT'));
 
 /**#@+
  * Authentication unique keys and salts.
